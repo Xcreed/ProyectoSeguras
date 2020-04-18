0a0b7d454162d65c71f473cd1a1f13edc88bf452
Xcreed <jpm311296@gmail.com>
2020-04-17 21:53:07 -0700

    main2

--- main2.php
+++ main2.php
@@ -4,7 +4,7 @@
         <title>Proyecto: Seguras</title>
         <script src="https://api.mapbox.com/mapbox-gl-js/v1.9.1/mapbox-gl.js"></script>
         <link href="https://api.mapbox.com/mapbox-gl-js/v1.9.1/mapbox-gl.css" rel="stylesheet" />
-        
+
         <!-- Latest compiled and minified CSS -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
 
@@ -13,24 +13,24 @@
 
         <!-- Latest compiled JavaScript -->
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-        
-          <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCZoly_sMdqz9uuxrol9pu1EGzJLLLOfh8&callback=initMap"
-            type="text/javascript"></script>
-        
+
+        <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCZoly_sMdqz9uuxrol9pu1EGzJLLLOfh8&callback=initMap"
+                type="text/javascript"></script>
+
         <style>
             body { margin:0; padding:0; }
-            
+
             #menu { width: 100%; height: 10%; display: table; table-layout: fixed; background-color:aliceblue; border: 10px; }
             #option1 { display: table-cell; vertical-align: middle; }
             #option2 { height: 100%; display: table-cell; vertical-align: middle; }
             #reportar { float: right; }
-            
+
             #map { position:absolute; bottom:0; width:100%; height: 90%; }
         </style>
     </head>
     <body>
         <!--<iframe src="https://www.google.com/maps/d/embed?mid=1np_UKeIdE8ICR42xlgvi-ILX8TVAUYVx" width="30%" height="30%"></iframe>-->
-        
+
         <div id="menu">
             <div id="option1">
                 <h1>Seguras</h1>
@@ -42,19 +42,25 @@
                 <a href="admin.php"><button id="adminLogin" name="adminLogin" class="btn btn-primary" value="Admin Login" onclick="adminLogin.php">Admin Login</button></a>
             </div>
         </div>
-        
+
         <div id="map"></div>
-    <script>
-// Initialize and add the map
-function initMap() {
-  // The location of Uluru
-  var uluru = {lat: -25.344, lng: 131.036};
-  // The map, centered at Uluru
-  var map = new google.maps.Map(
-      document.getElementById('map'), {zoom: 4, center: uluru});
-  // The marker, positioned at Uluru
-  var marker = new google.maps.Marker({position: uluru, map: map});
-}
+        <script>
+            function initMap() {
+                var uluru = {lat: -25.344, lng: 131.036};
+                var map = new google.maps.Map(
+                    document.getElementById('map'), {zoom: 4, center: uluru});
+                // The marker, positioned at Uluru
+                var marker = new google.maps.Marker({position: uluru, map: map});
+            }
+
+            function myfunction () {
+                var url = window.location.href;
+                url = url.replace("/main.php","/reportar.php");
+                url+= "?longitud=" + long + "&latitud=" + lat;
+                window.location.replace(url);
+                localStorage.setItem("mapa", map);
+            }
+
         </script>   
     </body>
 </html>
