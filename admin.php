61431daa907d2a43454b7a03bcb45ad7178f695e
Xcreed <jpm311296@gmail.com>
2020-04-18 00:10:44 -0700

    navbar

--- admin.php
+++ admin.php
@@ -20,19 +20,38 @@ if (!isset($_SESSION['usuarioValido']) or $_SESSION['usuarioValido'] == 0) {
 
         <!-- Latest compiled JavaScript -->
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+        <style>
 
-       
+
+            .navbar { margin-bottom: 0px; }
+
+        </style>
+
     </head>
     <body>
+
+        <nav class="navbar navbar-default" style="margin-bottom: 0px;">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <a class="navbar-brand" href="main.php">
+                        <img id="logo" src="logoSeg.PNG" width="30" height="30">
+                    </a>
+                    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                        <ul class="nav navbar-nav">
+                            <li class="active"><a href="admin.php">Reportes<span class="sr-only">(current)</span></a></li>
+                            <li class=""><a href="administrarAdmins.php">Administrar administradores<span class="sr-only"></span></a></li>
+                        </ul>
+                        <ul class="nav navbar-nav navbar-right">
+                            <li><a onclick="logout()">Cerrar Sesión</a></li>
+                        </ul>
+                    </div><!-- /.navbar-collapse -->
+                </div>
+            </div>
+        </nav>
 
         <div class="container">
 
             <?php echo  "<h1>Bienvenido " . $_SESSION['nombre'] . "!</h1>"; ?>
-
-            <a href="administrarAdmins.php"><label>Manejar Administradores</label></a>
-
-            <a href="adminLogin.php"><label>Cerrar Sesión</label><?php $_SESSION['logout'] = '1';?></a>
-
             <table style="padding=2px;"  class="table table-hover">
                 <thead>
                     <tr class="encabezado"> 
@@ -64,4 +83,15 @@ if (!isset($_SESSION['usuarioValido']) or $_SESSION['usuarioValido'] == 0) {
         </div>
     </body>
 </html>
+
+<script>
+    
+function logout () {
+               <?php $_SESSION['logout'] = 1;
+        ?>
+    var url = window.location.href;
+                url = url.replace("/admin.php","/adminLogin.php");
+                window.location.replace(url);
+            }
+</script>
 
