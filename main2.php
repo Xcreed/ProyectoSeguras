d56128e8e6be217b662ff258c4af01f038a43c9d
Xcreed <jpm311296@gmail.com>
2020-04-17 22:15:32 -0700

    Update main2.php

--- main2.php
+++ main2.php
@@ -36,7 +36,7 @@
                 <h1>Seguras</h1>
             </div>
             <div id="option2">
-                <button id="reportar" name="reportar" class="btn btn-primary" value="Reportar" onclick="myfunction()">Reportar</button>
+                <button id="reportar" name="reportar" class="btn btn-primary" value="Reportar" onclick="reportar()">Reportar</button>
             </div>
             <div id="option3">
                 <a href="admin.php"><button id="adminLogin" name="adminLogin" class="btn btn-primary" value="Admin Login" onclick="adminLogin.php">Admin Login</button></a>
@@ -45,26 +45,58 @@
 
         <div id="map"></div>
         <script>
+            var long = 0;
+            var lati = 0;
+            
             function initMap() {
-                var uluru = {lat: -25.344, lng: 131.036};
-                var map = new google.maps.Map(
-                    document.getElementById('map'), {zoom: 4, center: uluru});
-                // The marker, positioned at Uluru
-                var marker = new google.maps.Marker({position: uluru, map: map});
+                map = new google.maps.Map(document.getElementById('map'), {
+                    center: {lat: 9.991723, lng:-84.143453},
+                    zoom: 15,
+                    disableDefaultUI: true
+
+                });
+                infoWindow = new google.maps.InfoWindow;
+
+                // Try HTML5 geolocation.
+                if (navigator.geolocation) {
+                    navigator.geolocation.getCurrentPosition(function(position) {
+                        lati = position.coords.latitude;
+                        long = position.coords.longitude;
+                        var pos = {
+                            lat: position.coords.latitude,
+                            lng: position.coords.longitude
+                        };
+
+                        infoWindow.setPosition(pos);
+                        infoWindow.setContent('Ubicación Actual');
+                        infoWindow.open(map);
+                        map.setCenter(pos);
+                    }, function() {
+                        handleLocationError(true, infoWindow, map.getCenter());
+                    });
+                } else {
+                    // Browser doesn't support Geolocation
+                    handleLocationError(false, infoWindow, map.getCenter());
+                }
             }
 
-            function myfunction () {
+            function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+                infoWindow.setPosition(pos);
+                infoWindow.setContent(browserHasGeolocation ?
+                                      'Error: The Geolocation service failed.' :
+                                      'Error: Your browser doesn\'t support geolocation.');
+                infoWindow.open(map);
+            }
+            
+            function reportar() {
                 var url = window.location.href;
                 url = url.replace("/main.php","/reportar.php");
-                url+= "?longitud=" + long + "&latitud=" + lat;
+                url+= "?longitud=" + long + "&latitud=" + lati;
                 window.location.replace(url);
                 localStorage.setItem("mapa", map);
             }
+                
 
         </script>   
     </body>
 </html>
-
-<script>
-    alert("Para visualizar reportes en su ubicación actual, hacer click en el botón de localización en la esquina superior derecha del mapa.")
-</script>
